a55e49f3a184256c23d44561244940432d5453f5
Hasan <khassanbeck@gmail.com>
2019-07-25 11:35:02 +0500

    Initial Commit

---
+++ GitHub Repositories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Repositories
+//
+//  Created by Hasan on 7/25/19.
+//  Copyright © 2019 Hasan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GitHub RepositoriesTests/GitHub_RepositoriesTests.swift
@@ -0,0 +1,34 @@
+//
+//  GitHub_RepositoriesTests.swift
+//  GitHub RepositoriesTests
+//
+//  Created by Hasan on 7/25/19.
+//  Copyright © 2019 Hasan. All rights reserved.
+//
+
+import XCTest
+@testable import GitHub_Repositories
+
+class GitHub_RepositoriesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
